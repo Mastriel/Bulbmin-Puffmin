f4e1e955e6d1b9600f5b4e526495bd876f034c56
mastriel <winterapricots@gmail.com>
2024-12-19 12:58:07 -0700

    add github actions

--- Bulbmin/vite.config.ts
+++ Bulbmin/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig(async () : Promise<UserConfig> => ({
     envPrefix: ["VITE_", "TAURI_"],
     build: {
         // Tauri supports es2021
-        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari15",
         // don't minify for debug builds
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
         // produce sourcemaps for debug builds
